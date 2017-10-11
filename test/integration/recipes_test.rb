dadb21b18c92bbcb44522f76c55c5cd3d4c16c17
Tagma 1 <31788490+tagma-1@users.noreply.github.com>
2017-10-11 09:41:38 +0000

    Add recipes controller and index route

---
+++ test/integration/recipes_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class RecipesTest < ActionDispatch::IntegrationTest
+  
+  test "should get recipes index" do
+    get recipes_url
+    assert_response :success
+  end
+  
+  # test "the truth" do
+  #   assert true
+  # end
+end
