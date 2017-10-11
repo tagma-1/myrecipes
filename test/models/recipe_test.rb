e701bc88138eb8e36844f84cbff93a10ae01b83b
Tagma 1 <31788490+tagma-1@users.noreply.github.com>
2017-10-11 09:05:56 +0000

    Add one to many association

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,7 +3,13 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
   
   def setup
-    @recipe = Recipe.new(name: "Vegetable Soup", description: "Great soup recipe.")
+    @chef = Chef.new(chefname: "Test", email: "test@example.com")
+    @recipe = @chef.recipes.build(name: "Vegetable Soup", description: "Great soup recipe.")
+  end
+  
+  test "Recipe without chef should be invalid" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
   end
   
   test "Recipe should be valid" do
