6228caaccbf93999709ea854dc96a4011b74a311
Tagma 1 <31788490+tagma-1@users.noreply.github.com>
2017-10-15 03:24:28 +0000

    Add new recipe creation with tests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,24 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
   
+  def create
+    
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.first #to be updated once authentication has been added
+    
+    if @recipe.save
+      flash[:success] = "Recipe was created successfully."
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+    
+  end
+  
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
   
 end
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -31,10 +31,26 @@ class RecipesTest < ActionDispatch::IntegrationTest
   
   test "create new valid recipe" do
     get new_recipe_path
+    assert_template 'recipes/new'
+    name_of_recipe = 'Chicken sautee'
+    description_of_recipe = 'Add chicken, add vegetables, cook for twenty minutes, serve delicious meal'
+    assert_difference 'Recipe.count', 1 do
+      post recipes_path, params: { recipe: { name: name_of_recipe, description: description_of_recipe } }
+    end
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
+    assert_match description_of_recipe.capitalize, response.body
   end
   
   test "reject invalid recipe submissions" do
     get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: {name: " ", description: " "}}
+    end
+    assert_template 'recipes/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
   end
   
   # test "the truth" do
