11573f65c4693ce9f9c61d9a4a8309e7767c047d
Tagma 1 <31788490+tagma-1@users.noreply.github.com>
2017-10-16 00:52:43 +0000

    Refactor and clean layout

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,12 @@
 class RecipesController < ApplicationController
   
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+  
   def index
     @recipes = Recipe.all
   end
   
   def show
-    @recipe = Recipe.find(params[:id])
   end
   
   def new
@@ -26,12 +27,10 @@ class RecipesController < ApplicationController
   end
   
   def edit
-    @recipe = Recipe.find(params[:id])
   end
   
   def update
-    
-    @recipe = Recipe.find(params[:id])
+  
     if @recipe.update(recipe_params)
       flash[:success] = "Recipe was updated successfully."
       redirect_to recipe_path(@recipe)
@@ -42,13 +41,16 @@ class RecipesController < ApplicationController
   end
   
   def destroy
-    @recipe = Recipe.find(params[:id])
     @recipe.destroy
     flash[:success] = "Recipe deleted successfully."
     redirect_to recipes_path
   end
   
   private
+  
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
   
   def recipe_params
     params.require(:recipe).permit(:name, :description)
