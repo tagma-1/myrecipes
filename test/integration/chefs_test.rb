7bc596d356029ef568512be9fdd7073a83e2881e
tagma-1 <31788490+tagma-1@users.noreply.github.com>
2017-10-21 04:17:16 +0000

    Add chefs listing page with test

---
+++ test/integration/chefs_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class ChefsTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @chef_one = Chef.create!(chefname: "Dave", email: "dave@dave.com", password: "password", password_confirmation: "password")
+    @chef_two = Chef.create!(chefname: "Samantha", email: "sam@example.com", password: "password", password_confirmation: "password")
+  end
+  
+  test "should get chefs index" do
+    get chefs_url
+    assert_response :success
+  end
+  
+  test "should get chefs listing" do
+    get chefs_path
+    assert_template 'chefs/index'
+    assert_select "a[href=?]", chef_path(@chef_one), text: @chef_one.chefname
+    assert_select "a[href=?]", chef_path(@chef_two), text: @chef_two.chefname
+  end
+
+
+end
