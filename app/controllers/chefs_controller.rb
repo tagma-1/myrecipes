fcf2338d31736b9d5ca2c88dea7b5a15dcfffb5b
tagma-1 <31788490+tagma-1@users.noreply.github.com>
2017-10-21 05:19:06 +0000

    Add listing and pagination

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,7 +1,7 @@
 class ChefsController < ApplicationController
   
   def index
-    @chefs = Chef.all
+    @chefs = Chef.paginate(page: params[:page], per_page: 5)
   end
   
   def new
@@ -22,6 +22,7 @@ class ChefsController < ApplicationController
   
   def show
     @chef = Chef.find(params[:id])
+    @chef_recipes = @chef.recipes.paginate(page: params[:page], per_page: 5)
   end
   
   def edit
