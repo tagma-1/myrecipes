efd220811a58d2be79b9693c2e5a7292c35c8f2d
tagma-1 <31788490+tagma-1@users.noreply.github.com>
2017-11-11 05:55:12 +0000

    Add ingredient model and many-to-many association with recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,8 @@ class Recipe < ApplicationRecord
   validates :chef_id, presence: true
   belongs_to :chef
   default_scope -> { order(updated_at: :desc) }
+  
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
 end
 
